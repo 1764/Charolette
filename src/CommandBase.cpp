b507e9da00d5bf904d1204f9c50aa08f4fe4e0b8
Blaine Harper <danealue@gmail.com>
2015-05-08 22:52:32 -0500

    Update CommandBase.cpp

--- src/CommandBase.cpp
+++ src/CommandBase.cpp
@@ -5,8 +5,6 @@
 // Initialize a single static instance of all of your subsystems to NULL
 //ExampleSubsystem* CommandBase::examplesubsystem = NULL;
 DriveTrain* CommandBase::drive_train = NULL;
-LeftRiser* CommandBase::left_riser = NULL;
-RightRiser* CommandBase::right_riser = NULL;
 OI* CommandBase::oi = NULL;
 
 CommandBase::CommandBase(char const *name) :
@@ -25,8 +23,6 @@ void CommandBase::init()
 	// Create a single static instance of all of your subsystems. The following
 	// line should be repeated for each subsystem in the project.
 	//examplesubsystem = new ExampleSubsystem();
-	//drive_train = new DriveTrain();
-	left_riser = new LeftRiser();
-	//right_riser = new RightRiser();
+	drive_train = new DriveTrain();
 	oi = new OI();
 }
